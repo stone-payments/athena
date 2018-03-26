341687d30279671bb3d37b16c2e812159b426118
luiscaiaffa <luisfelipecaiaffa@gmail.com>
2018-03-26 14:12:20 -0300

    [feature] Create webhook

---
+++ webhook/tests.py
@@ -0,0 +1,11 @@
+from apistar.test import TestClient
+
+
+def test_http_request():
+    """
+    Testing a view, using the test client.
+    """
+    client = TestClient(app)
+    response = client.get('http://localhost/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Welcome to API Star!'}
